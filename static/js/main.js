7983986baffeb1c137912426a849e3557a1ce664
Mosesvalei <moses.mwanzia@students.moringaschool.com>
2021-06-24 01:55:53 +0300

    styled toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
-    let desp = $('.desp');
-    let devp = $('.devp');
-    let prop = $('.prop');
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
     let destext = $('#destxt');
     let devtext = $('#devtext');
     let protext = $('#protext');
